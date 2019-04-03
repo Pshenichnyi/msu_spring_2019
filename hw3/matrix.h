037b509ef3c1fc64068c5e7da949ec1354a3694c
Pshenichnyi <36666939+Pshenichnyi@users.noreply.github.com>
2019-04-04 00:31:11 +0300

    Update matrix.h

--- hw3/matrix.h
+++ hw3/matrix.h
@@ -4,66 +4,66 @@
 class Matrix
 {
 private:
-	int **matr;
-	int cols;
-	int rols;
+    int **matr;
+    int cols;
+    int rols;
 
-	class Array
-	{
-		int *arr;
-		int arr_len;
-	public:
-		Array(int *a, int len)
-		{
-			arr_len = len;
-			arr = a;
-		};
+    class Array
+    {
+        int *arr;
+        int arr_len;
+    public:
+        Array(int *a, int len)
+        {
+            arr_len = len;
+            arr = a;
+        };
 
-		int &operator[](int i) const
-		{
-			if(i < arr_len && i > -1)
-			{
-				return arr[i];
-			}
-			else 
-				throw std::out_of_range("");
-		}
-	};
+        int &operator[](int i) const
+        {
+            if(i < arr_len && i > -1)
+            {
+                return arr[i];
+            }
+            else 
+                throw std::out_of_range("");
+        }
+    };
 
 public:
-	Matrix(int x, int y)
-	{
+    Matrix(int x, int y)
+    {
         rols = x;
-		cols = y;
-		matr = new int* [rols];
-		for (int i = 0; i < rols; i ++)
-			matr[i] = new int[cols];
-	};
-	~Matrix()
-	{
-		for (int i = 0; i < rols; i ++)
-			delete [] matr[i];
-		delete [] matr;
-	}
-	int getRows() const
-	{
-		return cols;
-	}
-	int getColumns () const
-	{
-		return rols;
-	}
+        cols = y;
+        matr = new int* [rols];
+        for (int i = 0; i < rols; i ++)
+            matr[i] = new int[cols];
+    };
+    ~Matrix()
+    {
+        for (int i = 0; i < rols; i ++)
+            delete [] matr[i];
+        delete [] matr;
+    }
+    int getRows() const
+    {
+        return rows;
+    }
+    int getColumns () const
+    {
+        return cols;
+    }
 
-	const Array operator[](int j) const
-	{
-		if(j < rols && j >-1)
-		{
-			Array arr(matr[j], cols);
-			return arr;
-		}
-		else
-			throw std::out_of_range("");
-	}
+    const Array operator[](int j) const
+    {
+        if(j < rols && j >-1)
+        {
+            Array arr(matr[j], cols);
+            return arr;
+        }
+        else
+            throw std::out_of_range("");
+    }
 
 
     const int& operator()(int i, int j) const {
@@ -90,12 +90,12 @@ public:
         return !((*this) == matr_2);
     }
 
-	void operator *= (int C)
-	{
-		for (int i = 0; i < rols; i ++)
-			for (int j = 0; j < cols; j++)
-			{
-				(*this)[i][j] *= C;
-			}
-	}
+    void operator *= (int C)
+    {
+        for (int i = 0; i < rols; i ++)
+            for (int j = 0; j < cols; j++)
+            {
+                (*this)[i][j] *= C;
+            }
+    }
 };
